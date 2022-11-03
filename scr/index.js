1cfe64513c90b44349ef890e27bcbe839020518d
Yuliia Shevchenko-Kotenko <juliashk1990@gmail.com>
2022-11-03 15:36:17 -0500

    correct the temperature

--- scr/index.js
+++ scr/index.js
@@ -56,6 +56,8 @@ dateElement.innerHTML = formatDate(currentDate);
 function displayFahrenheitTemperature(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
   let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
